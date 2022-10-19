6f26c8c32653ecb14e7f034e48fd3fc212516013
ranjurave <ranjurave@gmail.com>
2022-10-19 23:14:46 +1300

    first commit

---
+++ ConsoleUser/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleUser.Models
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
